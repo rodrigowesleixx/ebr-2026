ce6ba857394d5b18014ce941efe46ce420da4d59
rodrigowesleixx <rodrigowesleixx@gmail.com>
2026-01-01 18:12:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('form-inscricao');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const nome = document.getElementById('nome').value;
+  const telefone = document.getElementById('telefone').value;
+  const email = document.getElementById('email').value;
+  const celula = document.getElementById('celula').value;
+  const lider = document.getElementById('lider').value;
+
+  const turmaSelecionada = document.querySelector('input[name="turma"]:checked');
+  const turma = turmaSelecionada ? turmaSelecionada.value : 'Não informado';
+
+  const numeroIgreja = '5585994215508';
+
+  const mensagem =
+    `Olá! Gostaria de me inscrever na *EBR – Escola Bíblica Restaurar*.%0A%0A` +
+        `🕘 Turma: ${turma}%0A` +
+        `👤 Nome: ${nome}%0A` +
+        `📱 WhatsApp: ${telefone}%0A` +
+        `📧 Email: ${email}%0A` +
+        `🏠 Célula: ${celula}%0A` +
+        `👥 Líder: ${lider}%0A`;
+
+  const url = `https://wa.me/${numeroIgreja}?text=${mensagem}`;
+
+  window.open(url, '_blank');
+});
